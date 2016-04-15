ca6e30c6f7a13c529c5c6013300e9b31ac1675c5
siri297 <siribhavana2@gmail.com>
2016-04-15 14:56:51 -0700

    Update Dice.java

--- Code/Dice.java
+++ Code/Dice.java
@@ -3,6 +3,13 @@ public class Dice
 {
 	public boolean past = false;
 	private int sum = 0;
+	/**add - The dice is rolled and random values are pushed in but only the even numbers will make the balloon pop.
+	 * The odd numbers of the dice will not do anything
+	 * Also the sum is thing which saves the added values
+	 * If the sum is lesser than 20 (which is the max limit to reach by any player) then the balloon dosen't pop
+	 * 
+	 * @param in, random values that could potentially be added to the score
+	 **/
 	public void add(int in)
 	{
 		if(in % 2==0)
@@ -23,7 +30,9 @@ public class Dice
 				System.out.print("The balloon is not popped");
 		}
 	}
-	
+	/** return- returns the calculated total that represents the players current score 
+	* @return int, returns the modified private instance variable of sum.
+	**/
 	public int returnSum()
 	{
 		return sum;
