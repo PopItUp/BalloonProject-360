b9180539853b3022e24eaf46b39d8272d681eb46
JRoldan267 <jroldan.267@gmail.com>
2016-04-15 21:48:19 -0700

    Update Player.java

--- Code/Player.java
+++ Code/Player.java
@@ -31,5 +31,273 @@ public class Player
 		
 		return value;
 	}
-
+	
+	public static void addWin(int play)
+	{
+		if(play == 1)
+			Pl1.incPops();
+		else if(play == 2)
+			Pl2.incPops();
+		else if(play == 3)
+			Pl3.incPops();
+		else if(play == 4)
+			Pl4.incPops();
+	}
+	
+	public static void addRoll(int play)
+	{
+		if(play == 1)
+			Pl1.incRolls();
+		else if(play == 2)
+			Pl2.incRolls();
+		else if(play == 3)
+			Pl3.incRolls();
+		else if(play == 4)
+			Pl4.incRolls();
+	}
+	
+	public static int getRolls(int play)
+	{
+		if(play == 1)
+			return Pl1.getRolls();
+		else if(play == 2)
+			return Pl2.getRolls();
+		else if(play == 3)
+			return Pl3.getRolls();
+		else if(play == 4)
+			return Pl4.getRolls();
+		return -1;
+	}
+	
+	public static int getRanks(int play)
+	{
+		if(play == 1)
+			return Pl1.getRanks();
+		else if(play == 2)
+			return Pl2.getRanks();
+		else if(play == 3)
+			return Pl3.getRanks();
+		else if(play == 4)
+			return Pl4.getRanks();
+		return -1;
+	}
+	
+	public static void setRanks(int play, int place)
+	{
+		if(play == 1)
+			Pl1.setRank(place);
+		else if(play == 2)
+			Pl2.setRank(place);
+		else if(play == 3)
+			Pl3.setRank(place);
+		else if(play == 4)
+			Pl4.setRank(place);
+	}
+	
+	public static void adjustRanks(int first)
+	{
+		if (first == 1)
+		{
+			if (Pl2.returnSum() > Pl3.returnSum())
+			{
+				if (Pl3.returnSum() > Pl4.returnSum())
+				{
+					Pl2.setRank(2);
+					Pl3.setRank(3);
+					Pl4.setRank(4);
+				}
+				
+				else if (Pl2.returnSum() > Pl4.returnSum())
+				{
+					Pl2.setRank(2);
+					Pl3.setRank(4);
+					Pl4.setRank(3);
+				}
+				
+				else
+				{
+					Pl2.setRank(3);
+					Pl3.setRank(4);
+					Pl4.setRank(2);
+				}
+			}
+			
+			else if (Pl3.returnSum() > Pl2.returnSum())
+			{
+				if (Pl2.returnSum() > Pl4.returnSum())
+				{
+					Pl2.setRank(3);
+					Pl3.setRank(2);
+					Pl4.setRank(4);
+				}
+				
+				else if (Pl4.returnSum() > Pl3.returnSum())
+				{
+					Pl2.setRank(4);
+					Pl3.setRank(3);
+					Pl4.setRank(2);
+				}
+				
+				else
+				{
+					Pl2.setRank(4);
+					Pl3.setRank(2);
+					Pl4.setRank(3);
+				}
+			}
+		}
+		
+		else if (first == 2)
+		{
+			if (Pl1.returnSum() > Pl3.returnSum())
+			{
+				if (Pl3.returnSum() > Pl4.returnSum())
+				{
+					Pl1.setRank(2);
+					Pl3.setRank(3);
+					Pl4.setRank(4);
+				}
+				
+				else if (Pl1.returnSum() > Pl4.returnSum())
+				{
+					Pl1.setRank(2);
+					Pl3.setRank(4);
+					Pl4.setRank(3);
+				}
+				
+				else
+				{
+					Pl1.setRank(3);
+					Pl3.setRank(4);
+					Pl4.setRank(2);
+				}
+			}
+			
+			else if (Pl3.returnSum() > Pl1.returnSum())
+			{
+				if (Pl1.returnSum() > Pl4.returnSum())
+				{
+					Pl1.setRank(3);
+					Pl3.setRank(2);
+					Pl4.setRank(4);
+				}
+				
+				else if (Pl4.returnSum() > Pl3.returnSum())
+				{
+					Pl1.setRank(4);
+					Pl3.setRank(3);
+					Pl4.setRank(2);
+				}
+				
+				else
+				{
+					Pl1.setRank(4);
+					Pl3.setRank(2);
+					Pl4.setRank(3);
+				}
+			}
+		}
+		
+		else if (first == 3)
+		{
+			if (Pl2.returnSum() > Pl1.returnSum())
+			{
+				if (Pl1.returnSum() > Pl4.returnSum())
+				{
+					Pl2.setRank(2);
+					Pl1.setRank(3);
+					Pl4.setRank(4);
+				}
+				
+				else if (Pl2.returnSum() > Pl4.returnSum())
+				{
+					Pl2.setRank(2);
+					Pl1.setRank(4);
+					Pl4.setRank(3);
+				}
+				
+				else
+				{
+					Pl2.setRank(3);
+					Pl1.setRank(4);
+					Pl4.setRank(2);
+				}
+			}
+			
+			else if (Pl1.returnSum() > Pl2.returnSum())
+			{
+				if (Pl2.returnSum() > Pl4.returnSum())
+				{
+					Pl2.setRank(3);
+					Pl1.setRank(2);
+					Pl4.setRank(4);
+				}
+				
+				else if (Pl4.returnSum() > Pl1.returnSum())
+				{
+					Pl2.setRank(4);
+					Pl1.setRank(3);
+					Pl4.setRank(2);
+				}
+				
+				else
+				{
+					Pl2.setRank(4);
+					Pl1.setRank(2);
+					Pl4.setRank(3);
+				}
+			}
+		}
+		
+		else if (first == 4)
+		{
+			if (Pl2.returnSum() > Pl3.returnSum())
+			{
+				if (Pl3.returnSum() > Pl1.returnSum())
+				{
+					Pl2.setRank(2);
+					Pl3.setRank(3);
+					Pl1.setRank(4);
+				}
+				
+				else if (Pl2.returnSum() > Pl1.returnSum())
+				{
+					Pl2.setRank(2);
+					Pl3.setRank(4);
+					Pl1.setRank(3);
+				}
+				
+				else
+				{
+					Pl2.setRank(3);
+					Pl3.setRank(4);
+					Pl1.setRank(2);
+				}
+			}
+			
+			else if (Pl3.returnSum() > Pl2.returnSum())
+			{
+				if (Pl2.returnSum() > Pl1.returnSum())
+				{
+					Pl2.setRank(3);
+					Pl3.setRank(2);
+					Pl1.setRank(4);
+				}
+				
+				else if (Pl1.returnSum() > Pl3.returnSum())
+				{
+					Pl2.setRank(4);
+					Pl3.setRank(3);
+					Pl1.setRank(2);
+				}
+				
+				else
+				{
+					Pl2.setRank(4);
+					Pl3.setRank(2);
+					Pl1.setRank(3);
+				}
+			}
+		}
+	}	
 }
