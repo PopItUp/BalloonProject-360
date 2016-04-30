b8ddfc8c220eb33442fb966730710823eb428066
Anisha Gupta <ani6gup@gmail.com>
2016-04-29 21:55:35 -0700

    Update StartPanel.java

--- Code/StartPanel.java
+++ Code/StartPanel.java
@@ -1,46 +1,101 @@
-import java.awt.Font;
-import java.awt.GridLayout;
+package me;
 
-import javax.swing.Box;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
-/** CSE 360 Project - StartPanel class
-* @author PopItUp
-* Description:StartPanel is a panel that features the title of the 
-* program, a GIF, and a direction to prompt the user to proceed to the game
-*
-*/
-public class StartPanel extends JPanel
-{
+public class StartPanel extends JPanel {
+	private JTextField userTextField, passTextField;
+	private JLabel titleLabel, balloonGif, startLabel, userLabel, passLabel; 
+	private JButton loginButton; 
 
-	private JPanel wholePanel;
-	private JLabel welcomeMessage, startMessage, picture;
-
-	/** StartPanel - constructor that initializes private instance variables**/
-	public StartPanel()
-	{
-		wholePanel  = new JPanel();
-
-		welcomeMessage = new JLabel(" PopItUp!");
-		welcomeMessage.setFont(new Font("Serif", Font.BOLD, 80));
+	/**
+	 * Create the panel.
+	 */
+	public StartPanel() {
+		setBackground(new Color(51, 153, 255));
 		
-		picture = new JLabel(new ImageIcon("balloons.gif"));
+		titleLabel = new JLabel("Pop It Up!");
+		titleLabel.setForeground(new Color(255, 255, 255));
+		titleLabel.setFont(new Font("Marker Felt", Font.PLAIN, 62));
 		
-		startMessage = new JLabel("Press Start to begin: ");
-		startMessage.setFont(new Font("Serif", Font.BOLD, 30));
+		balloonGif = new JLabel("Gif Here\n");
 		
-		wholePanel.add(welcomeMessage, Box.CENTER_ALIGNMENT);
-		setLayout(new GridLayout(3,1));
-		add(wholePanel);
-		add(picture);
-		add(startMessage);
+		startLabel = new JLabel("Press Start to Begin! ");
+		startLabel.setForeground(Color.WHITE);
+		startLabel.setFont(new Font("Marker Felt", Font.PLAIN, 23));
 		
+		userTextField = new JTextField();
+		userTextField.setColumns(10);
+		
+		passTextField = new JTextField();
+		passTextField.setColumns(10);
+		
+		loginButton = new JButton("Login");
+		
+		userLabel = new JLabel("User:\n");
+		userLabel.setForeground(Color.WHITE);
+		userLabel.setFont(new Font("Marker Felt", Font.PLAIN, 13));
+		
+		passLabel = new JLabel("Pass:");
+		passLabel.setForeground(Color.WHITE);
+		passLabel.setFont(new Font("Marker Felt", Font.PLAIN, 13));
+		
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap(115, Short.MAX_VALUE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(58)
+									.addComponent(startLabel))
+								.addComponent(balloonGif, GroupLayout.PREFERRED_SIZE, 345, GroupLayout.PREFERRED_SIZE))
+							.addGap(171))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(userLabel)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(userTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(70)
+							.addComponent(passLabel, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(passTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(62)
+							.addComponent(loginButton)
+							.addGap(46))))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(229)
+					.addComponent(titleLabel)
+					.addContainerGap(250, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(27)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+							.addComponent(loginButton)
+							.addComponent(passTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addComponent(userTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addComponent(userLabel, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE))
+						.addComponent(passLabel, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE))
+					.addGap(26)
+					.addComponent(titleLabel)
+					.addGap(10)
+					.addComponent(balloonGif, GroupLayout.PREFERRED_SIZE, 165, GroupLayout.PREFERRED_SIZE)
+					.addGap(16)
+					.addComponent(startLabel, GroupLayout.PREFERRED_SIZE, 68, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(84, Short.MAX_VALUE))
+		);
+		setLayout(groupLayout);
 
 	}
-	
-
-} //end of StartPanel class
-
-
+}
